8b309cf72c48d3c67d2a64a015ea31ec94bbf9b7
irekrog <irekfrontend@gmail.com>
2016-09-17 11:55:07 +0200

    simple messaging, add iframe

--- script.js
+++ script.js
@@ -1,16 +1,21 @@
-
 const channel = new BroadcastChannel('sample_channel');
-
 let send = document.querySelector(".send");
+let messageText = document.querySelector(".message-text");
+let messageContent = document.querySelector(".message-content");
+
+let href = location.pathname.substring(location.pathname.lastIndexOf("/") + 1);
 
 send.addEventListener("click", () => {
-
-	channel.postMessage('This is a message.');
-	
+	channel.postMessage(href + ": " + "<span>" + messageText.value + "</span>");
+	createMessage("You: " + messageText.value);
 }, false);
 
 channel.addEventListener("message", (e) => {
+	createMessage(e.data);
+}, false);
 
-  console.log('Received message: ', e.data);
-
-});
+function createMessage(message) {
+	let item = document.createElement("P");
+	item.innerHTML = message;
+	messageContent.appendChild(item);
+}
